98241d9a7886ac37804970e482b1370fdaff9b53
Lesnov Petr <lesnov,p@gmail.com>
2021-08-22 20:35:05 +0300

    Update text-api.jsx

--- src/servicesAPI/text-api.jsx
+++ src/servicesAPI/text-api.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
-axios.defaults.baseURL = `http://tmgwebtest.azurewebsites.net/api`;
+
+const cors = "https://cors-anywhere.herokuapp.com/";
+axios.defaults.baseURL = `${cors}http://tmgwebtest.azurewebsites.net/api`;
 // axios.defaults.headers.common["TMG-Api-Key"] = "0J/RgNC40LLQtdGC0LjQutC4IQ==";
 
 const serviceAPI = {
   getTextList(listID) {
     return axios.get(`/textstrings/${listID}`, {
       headers: {
+        Accept: "aplication/json",
         "TMG-Api-Key": "0J/RgNC40LLQtdGC0LjQutC4IQ==",
+        // "Content-Type": "multipart/form-data",
       },
-      mode: "no-cors",
     });
   },
 };
